6efcc5e311df0b5510576648680fd80134ad9ae8
Armin Biere <armin.biere@gmail.com>
2025-03-27 10:03:24 +0100

    added README and configure for build support

---
+++ README.md
@@ -0,0 +1,6 @@
+Normalize CNF
+-------------
+
+This is a tool to normalize CNF in DIMACS format.
+
+Run `./configure && make` to build `normalize-cnf`.
--- normalize-cnf.c
+++ normalize-cnf.c
@@ -31,7 +31,7 @@ static const char *input_path, *output_path;
 static FILE *input_file, *output_file;
 static bool close_input, close_output;
 
-static void die(const char* fmt, ...) {
+static void die(const char *fmt, ...) {
   fprintf(stderr, "normalize: error in '%s': ", input_path);
   va_list ap;
   va_start(ap, fmt);
@@ -41,9 +41,9 @@ static void die(const char* fmt, ...) {
   exit(1);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   for (int i = 1; i != argc; i++) {
-    const char* arg = argv[i];
+    const char *arg = argv[i];
     if (!strcmp(arg, "-h")) {
       fputs(usage, stdout);
       exit(0);
@@ -65,41 +65,49 @@ int main(int argc, char** argv) {
     while ((ch = getc(input_file)) != '\n')
       if (ch == EOF)
       END_OF_FILE_IN_COMMENT:
-	die("end-of-file in comment");
-  if (ch != 'p') die("expected 'p cnf ...' header or 'c' comment");
-  for (const char* p = " cnf "; *p; p++)
-    if (*p != getc(input_file)) die("invalid 'p cnf ...' header");
+        die("end-of-file in comment");
+  if (ch != 'p')
+    die("expected 'p cnf ...' header or 'c' comment");
+  for (const char *p = " cnf "; *p; p++)
+    if (*p != getc(input_file))
+      die("invalid 'p cnf ...' header");
   ch = getc(input_file);
   if (!isdigit(ch))
   INVALID_VARIABLES:
     die("invalid number of variables");
   int variables = ch - '0';
   while (isdigit(ch = getc(input_file))) {
-    if (INT_MAX / 10 < variables) goto INVALID_VARIABLES;
+    if (INT_MAX / 10 < variables)
+      goto INVALID_VARIABLES;
     variables *= 10;
     int digit = ch - '0';
-    if (INT_MAX - digit < variables) goto INVALID_VARIABLES;
+    if (INT_MAX - digit < variables)
+      goto INVALID_VARIABLES;
     variables += digit;
   }
-  if (ch != ' ') die("expected space in header after variables");
+  if (ch != ' ')
+    die("expected space in header after variables");
   ch = getc(input_file);
   if (!isdigit(ch))
   INVALID_CLAUSES:
     die("invalid number of clauses");
   int clauses = ch - '0';
   while (isdigit(ch = getc(input_file))) {
-    if (INT_MAX / 10 < clauses) goto INVALID_CLAUSES;
+    if (INT_MAX / 10 < clauses)
+      goto INVALID_CLAUSES;
     clauses *= 10;
     int digit = ch - '0';
-    if (INT_MAX - digit < clauses) goto INVALID_CLAUSES;
+    if (INT_MAX - digit < clauses)
+      goto INVALID_CLAUSES;
     clauses += digit;
   }
-  if (ch == '\r') ch = getc(input_file);
+  if (ch == '\r')
+    ch = getc(input_file);
   if (ch == ' ' || ch == '\t') {
     while ((ch = getc(input_file)) != '\n')
       if (ch != ' ' && ch != '\t' && ch != '\r')
       EXPECTED_NEW_LINE_AFTER_HEADER:
-	die("expected white-space and a new-line after clauses");
+        die("expected white-space and a new-line after clauses");
   } else if (ch != '\n')
     goto EXPECTED_NEW_LINE_AFTER_HEADER;
   if (!output_path || !strcmp(output_path, "-"))
@@ -113,17 +121,22 @@ int main(int argc, char** argv) {
   for (;;) {
     ch = getc(input_file);
     if (ch == EOF) {
-      if (lit) die("zero at end of last clause missing");
-      if (parsed < clauses) die("clause missing");
+      if (lit)
+        die("zero at end of last clause missing");
+      if (parsed < clauses)
+        die("clause missing");
       break;
     }
     if (ch == 'c') {
       while ((ch = getc(input_file)) != '\n')
-	if (ch == EOF) goto END_OF_FILE_IN_COMMENT;
+        if (ch == EOF)
+          goto END_OF_FILE_IN_COMMENT;
       continue;
     }
-    if (ch == '\r') ch = getc(input_file);
-    if (ch == ' ' || ch == '\n' || ch == '\t') continue;
+    if (ch == '\r')
+      ch = getc(input_file);
+    if (ch == ' ' || ch == '\n' || ch == '\t')
+      continue;
     int sign = 1;
     if (ch == '-') {
       ch = getc(input_file);
@@ -134,19 +147,24 @@ int main(int argc, char** argv) {
       die("invalid literal");
     lit = ch - '0';
     while (isdigit(ch = getc(input_file))) {
-      if (INT_MAX / 10 < lit) goto INVALID_LITERAL;
+      if (INT_MAX / 10 < lit)
+        goto INVALID_LITERAL;
       lit *= 10;
       int digit = ch - '0';
-      if (INT_MAX - digit < lit) goto INVALID_LITERAL;
+      if (INT_MAX - digit < lit)
+        goto INVALID_LITERAL;
       lit += digit;
     }
-    if (lit > variables) goto INVALID_LITERAL;
-    if (ch == '\r') ch = getc(input_file);
+    if (lit > variables)
+      goto INVALID_LITERAL;
+    if (ch == '\r')
+      ch = getc(input_file);
     if (ch != ' ' && ch != '\n' && ch != '\t' && ch != 'c' && ch != EOF)
       die("expected white-space after literal");
     if (ch == 'c') {
       while ((ch = getc(input_file)) != '\n')
-	if (ch == EOF) goto END_OF_FILE_IN_COMMENT;
+        if (ch == EOF)
+          goto END_OF_FILE_IN_COMMENT;
     }
     if (lit)
       fprintf(output_file, "%d ", sign * lit);
@@ -155,7 +173,9 @@ int main(int argc, char** argv) {
     else
       fputs("0\n", output_file);
   }
-  if (close_input) fclose(input_file);
-  if (close_output) fclose(output_file);
+  if (close_input)
+    fclose(input_file);
+  if (close_output)
+    fclose(output_file);
   return 0;
 }
